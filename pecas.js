705fdf831bb6a6d42aa5f2417bea6fe1b07a908a
Luciano-do-Carmo <lucianodocarmo111@gmail.com>
2022-01-09 16:57:01 -0300

    adicionando arquivos pecas e exercicios

---
+++ pecas.js
@@ -0,0 +1,24 @@
+//variaveis, sao espacos na memoria para guardar algum valor
+let pesoPeca = 110
+
+if(pesoPeca > 100) {
+    console.log("peso maior que 100g, logo, podemos cadastrar a peca!!!")
+} else { 
+    console.log(" Peso insuficente, nao e possivel cadastrar.")
+}
+
+let numeropecas = 10
+if(numeropecas < 10) {
+    console.log("ainda ha espaco na caixa, podemos cadastrar a peca.")
+
+}else {
+    console.log("nao ha espaco disponvel na caixa")
+}
+
+let nomepeca = "calota"
+console.log("o comprimento do nome da peca e", nomepeca. comprimento)
+if(nomepeca. comprimento < 3) {
+    console.log("nao e possivel cadastrar, pois o o nome e muito curto")
+} else {
+    console.log("nome da peca esta adequado, podemos cadastrar")
+}
